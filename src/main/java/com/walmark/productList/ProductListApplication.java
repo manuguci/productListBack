3af12af3a0cf5bf584c011392a3728e72d3e2730
Manuel Gutierrez <manuel.gutierrez@zentragroup.com>
2020-10-01 00:17:32 -0300

    Heroku java version

--- src/main/java/com/walmark/productList/ProductListApplication.java
+++ src/main/java/com/walmark/productList/ProductListApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 public class ProductListApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(ProductListApplication.class, args);
 	}
 	
